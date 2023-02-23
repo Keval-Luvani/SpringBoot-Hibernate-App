156850dfcd02956b5a2aef99faed32b13f3493cb
Keval-Luvani <keval@webosmotic@gmail.com>
2023-02-23 19:07:38 +0530

    empty springboot-hibernate app

---
+++ src/main/java/com/keval/SpringHibernateApp/SpringHibernateAppApplication.java
@@ -0,0 +1,13 @@
+package com.keval.SpringHibernateApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keval/SpringHibernateApp/SpringHibernateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keval.SpringHibernateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
